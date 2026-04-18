1ea96e7550b345e88b5bbd4118e74b6e7f276f19
TheUltimateWarMachine <72458286+TheUltimateWarMachine@users.noreply.github.com>
2026-04-17 21:10:01 -0700

    Update main.c

--- botguy_bot/main.c
+++ botguy_bot/main.c
@@ -11,22 +11,21 @@
 #define BLACK_THRESHOLD 1200 //any tophat reading past this is black.
 #include"library.h"
 #include"instructions.h"
+int start_seconds;
 //Each step of the robot's movement should be in instructions.h!
 //Do not clutter this file.
 int main() {
-    //square_up_smooth(1300);
-    //drive_smooth(2600);
-    // straight_square_up(1500, 2000);
-    rotate_smooth(90); // Turn right towards middle of board
-    halt_robot("Testing ended");
-    
+    start_seconds = seconds();
     shut_down_in(119);
     enable_servos();
     claw_reset();
+    printf("Starting drive to green cube\n");
     drive_to_green_cube_from_starting_box();
+    //home_drive_to_green_cube_from_starting_box();
     knock_green_cube(); 
     open_warehouse_doors();
     grab_botguy();
+    drive_back_to_starting_area();
     halt_robot("Program ended");
     return EXIT_SUCCESS;
 }
